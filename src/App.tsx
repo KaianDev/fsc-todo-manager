808b3b3f1b125f9171bd59eeb671babe907791c8
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-07-03 11:45:01 -0300

    chore: add prettier-plugin-tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,14 @@ const App = () => {
   const name = ''
   const b = ''
   console.log(name, b)
-  return <div>Hello World</div>
+  return (
+    <div>
+      Hello World
+      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        ...
+      </button>
+    </div>
+  )
 }
 
 export default App
