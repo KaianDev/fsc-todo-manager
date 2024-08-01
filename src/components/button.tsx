92c859b364ed217d6dedcea9d48c13c8e9c27f76
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-08-01 18:57:35 -0300

    refactor: add size and secondary variant to Button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,11 +1,13 @@
 interface Props extends React.ComponentProps<'button'> {
   children: React.ReactNode
-  variant?: 'primary' | 'ghost'
+  variant?: 'primary' | 'ghost' | 'secondary'
+  size?: 'small' | 'large'
 }
 
 const Button = ({
   children,
   variant = 'primary',
+  size = 'small',
   className,
   ...rest
 }: Props) => {
@@ -14,11 +16,24 @@ const Button = ({
       return 'bg-[#00ADB5] text-white hover:bg-[#00ADB5]/80'
     if (variant === 'ghost')
       return 'bg-transparent text-[#818181] hover:bg-[#F0F0F0]'
+
+    if (variant === 'secondary')
+      return 'bg-[#EEEEEE] text-[#35383E] hover:bg-[#DEDEDE]'
+  }
+
+  const getSizeClasses = () => {
+    if (size === 'small') {
+      return ' py-1 text-xs'
+    }
+
+    if (size === 'large') {
+      return 'py-2 text-sm'
+    }
   }
 
   return (
     <button
-      className={`flex items-center gap-1 rounded-md px-3 py-1 text-xs font-semibold transition-colors ${getVariantClasses()} ${className}`}
+      className={`flex items-center justify-center gap-1 rounded-md px-3 font-semibold transition-colors ${getVariantClasses()} ${getSizeClasses()} ${className}`}
       {...rest}
     >
       {children}
